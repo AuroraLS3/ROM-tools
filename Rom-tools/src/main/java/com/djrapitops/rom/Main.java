ab8160a1eb82553400395c6277992e594ce625d5
Rsl1122 <djrapitops@hotmail.com>
2018-03-19 12:32:08 +0200

    Initialized Maven Project

---
+++ Rom-tools/src/main/java/com/djrapitops/rom/Main.java
@@ -0,0 +1,14 @@
+package com.djrapitops.rom;
+
+/**
+ * Main class of ROM-tools, launches backend threads & UI thread.
+ *
+ * @author Rsl1122
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
