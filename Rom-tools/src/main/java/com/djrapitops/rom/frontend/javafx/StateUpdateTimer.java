125916339d85a04874f8c8a601f6bba6ebd498fe
Rsl1122 <djrapitops@hotmail.com>
2018-05-05 17:33:18 +0300

    Fixed view buttons being unresponsive

--- Rom-tools/src/main/java/com/djrapitops/rom/frontend/javafx/StateUpdateTimer.java
+++ Rom-tools/src/main/java/com/djrapitops/rom/frontend/javafx/StateUpdateTimer.java
@@ -3,7 +3,6 @@ package com.djrapitops.rom.frontend.javafx;
 import com.djrapitops.rom.frontend.state.State;
 import com.djrapitops.rom.frontend.state.Updatable;
 import javafx.animation.AnimationTimer;
-import javafx.application.Platform;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +31,7 @@ public class StateUpdateTimer extends AnimationTimer {
         // New List to prevent Concurrent Exception due to GamesView adding more games that are updatable.
         List<Updatable<State>> elementsToUpdate = new ArrayList<>(state.getUpdateOnChange());
 
-        Platform.runLater(() -> elementsToUpdate.forEach(toUpdate -> toUpdate.update(state)));
+        elementsToUpdate.forEach(toUpdate -> toUpdate.update(state));
         lastUpdate = now;
     }
 }
